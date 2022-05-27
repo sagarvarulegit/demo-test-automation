e9e2ec9cae4ef390d2b38ba5d004865e55526870
sagar varule <sagar.varule@gmail.com>
2022-05-27 12:50:08 +0530

    resolve error with driver

--- sample-test-java/src/test/java/LoginTest.java
+++ sample-test-java/src/test/java/LoginTest.java
@@ -10,17 +10,23 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 
 import org.openqa.selenium.remote.RemoteWebDriver;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
 public class LoginTest {
-    static WebDriver driver;
+    static ChromeDriver driver;
 
     @Test
     public void TestVerifyLoginTest() throws IOException {
         ChromeOptions options = new ChromeOptions();
-        driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+        options.addArguments("--headless");
+        // driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver(options);
         driver.get("http://localhost:1234/sample-login.html");
         driver.findElement(By.id("input_username")).clear();
         driver.findElement(By.id("input_username")).sendKeys("Sagar");
