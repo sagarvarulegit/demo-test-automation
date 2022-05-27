7d80152d6772d97f1749946eafbcef67a1388d41
sagar varule <sagar.varule@gmail.com>
2022-05-27 15:10:36 +0530

    change server to node.js

--- sample-test-java/src/test/java/LoginTest.java
+++ sample-test-java/src/test/java/LoginTest.java
@@ -9,10 +9,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
-
 import org.openqa.selenium.chrome.ChromeOptions;
-
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class LoginTest {
@@ -20,14 +17,15 @@ public class LoginTest {
 
     @Test
     public void TestVerifyLoginTest() throws IOException {
-        #sample test
+        //sample test
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless");
         // driver = new RemoteWebDriver(new URL("http://localhost:4444"), options);
         WebDriverManager wdm = WebDriverManager.chromedriver().browserInDocker()
             .enableVnc().enableRecording();
         driver = wdm.create();
-        driver.get("http://192.168.0.183:1234/sample-login.html");
+        driver.get("http://127.0.0.1:8080/sample-login.html");
+        System.out.println(driver.getPageSource());
         driver.findElement(By.id("input_username")).clear();
         driver.findElement(By.id("input_username")).sendKeys("Sagar");
 
